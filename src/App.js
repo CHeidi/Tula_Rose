17866e163ffc8336ded44f5b961d504aa2354ade
CHeidi <hcfcodes@gmail.com>
2021-02-09 14:12:33 -0600

    start of components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
+    <Header />
+    <main>
+      <Container>
       <h1>Welcome to Tula Rose</h1>
+      </Container>
+    </main>
+      
+    <Footer />
     </>
   );
 }
